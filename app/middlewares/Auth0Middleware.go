542fe49653dd047ff6ca7f38352037d38a343669
hirsch88 <gery.hirschfeld@w3tec.ch>
2017-09-01 07:42:58 +0200

    Add local endpoint through .env

--- app/middlewares/Auth0Middleware.go
+++ app/middlewares/Auth0Middleware.go
@@ -1,13 +1,15 @@
 package middlewares
 
 import (
+	"os"
+
 	auth0middleware "github.com/w3tecch/go-auth0-middleware"
 )
 
 // Auth0Middleware ...
 func Auth0Middleware() *auth0middleware.Auth0Middleware {
 	return auth0middleware.New(auth0middleware.Options{
-		Endpoint:   "qta.eu.auth0.com",
+		Endpoint:   os.Getenv("AUTH0_BASE_URL"),
 		ContextKey: "TokenInfo",
 	})
 }
