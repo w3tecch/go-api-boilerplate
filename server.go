9571d659aa4e10a6548f1911558f8b6a7e7e6854
hirsch88 <gery.hirschfeld@w3tec.ch>
2017-09-22 17:14:17 +0200

    Add banner back to server start

--- server.go
+++ server.go
@@ -2,28 +2,31 @@ package main
 
 import (
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/sirupsen/logrus"
 
 	"github.com/w3tecch/go-api-boilerplate/app"
+	"github.com/w3tecch/go-api-boilerplate/config/env"
+	"github.com/w3tecch/go-api-boilerplate/config/logger"
 )
 
 // ServerLog ...
-// var ServerLog = config.Logger{Scope: "server"}
+var ServerLog = logger.Logger{Scope: "server"}
 
 func main() {
 
 	// Prepars server before running it
 	app.Setup()
 
-	// // Prints the version and the address of our api to the console
-	// ServerLog.LineBreak()
-	// ServerLog.WithFields(logrus.Fields{
-	// 	"title":    config.Env().APITitle,
-	// 	"version":  config.Env().APIVersion,
-	// 	"env":      config.Env().Name,
-	// 	"gin_mode": config.Env().GinMode,
-	// }).Info("API information")
-	// ServerLog.Info("Starting Server on http://localhost" + config.Env().APIPort)
-	// ServerLog.LineBreak()
+	// Prints the version and the address of our api to the console
+	ServerLog.LineBreak()
+	ServerLog.WithFields(logrus.Fields{
+		"title":    env.Get().APITitle,
+		"version":  env.Get().APIVersion,
+		"env":      env.Get().Name,
+		"gin_mode": env.Get().GinMode,
+	}).Info("API information")
+	ServerLog.Info("Starting Server on http://localhost" + env.Get().APIPort)
+	ServerLog.LineBreak()
 
 	// Listen on the given port on localhost
 	app.Run()
